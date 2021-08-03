ead91b4618374048f9555c9ea76bf2c7e5ee19e1
Petyo Milotinov <petyo@securax.org>
2021-08-03 12:23:35 +0300

    Switch AGC and Echo Cancellation to software mode.

--- ZDKDemo/ContextManager.swift
+++ ZDKDemo/ContextManager.swift
@@ -66,8 +66,8 @@ class ContextManager: NSObject {
             zdkContext.configuration.rtpPort = Int32.random(in: 32000..<65000)
         } while (zdkContext.configuration.rtpPort != zdkContext.configuration.sipUdpPort)
         
-        zdkContext.audioControls.setAutomaticGainMode(.agct_Hardware, gain: 0.0)
-        zdkContext.audioControls.echoCancellation     = .ect_Hardware
+        zdkContext.audioControls.setAutomaticGainMode(.agct_SoftwareAnalog, gain: 0.0)
+        zdkContext.audioControls.echoCancellation     = .ect_Software
         zdkContext.audioControls.noiseSuppression     = true
         zdkContext.configuration.enableIPv6           = true
     }
